cc75795951d064361d46328721fae51fb6bf11e2
Sven Kirmess <sven.kirmess@kzone.ch>
2017-10-21 11:54:39 +0200

    0.005

--- lib/Dist/Zilla/Plugin/AutoPrereqs/Perl/Critic.pm
+++ lib/Dist/Zilla/Plugin/AutoPrereqs/Perl/Critic.pm
@@ -4,7 +4,7 @@ use 5.006;
 use strict;
 use warnings;
 
-our $VERSION = '0.004';
+our $VERSION = '0.005';
 
 use Moose;
 with qw(
@@ -163,7 +163,7 @@ Dist::Zilla::Plugin::AutoPrereqs::Perl::Critic - automatically extract Perl::Cri
 
 =head1 VERSION
 
-Version 0.004
+Version 0.005
 
 =head1 SYNOPSIS
 
